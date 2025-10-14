a66f7317e47a14368246424e2bb37fbde3e8f43b
Bernard Murphy <bernard.murphy@rutgers.edu>
2025-10-14 19:06:32 +0800

    misc server redirect

--- misc/server.js
+++ misc/server.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 6363;
 
 app.use(express.static(path.join(__dirname, "..", "ui", "dist")));
 
+app.use((req, res) => res.redirect("/"));
+
 const server = http.createServer(app);
 
 server.listen(port, () => console.log("sg running on port", port));
